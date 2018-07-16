cce7b7bc279834983744b400cd8d0549ba974812
Clay Dunston <dunstontc@gmail.com>
2018-07-16 15:09:07 -0500

    scss

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ import Footer from './layout/Footer';
 class Home extends React.Component {
   render() {
     return (
-        <div>
+        <div className="flex-container">
           <Header/>
           <Sidebar/>
           <Footer/>
